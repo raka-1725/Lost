6c256df3731b4cec472a87fb4cb6388394ab83d1
Ryoya Akaike <akaike@student.uiwtx.edu>
2025-10-13 16:08:41 -0500

    Ballte party sysem initial setup, Oct 13

--- Assets/_Assets/Battle/BattleManager.cs
+++ Assets/_Assets/Battle/BattleManager.cs
@@ -1,9 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleManager
 {
-    public void StartBattle(BattlePartyComponent partyOne, BattlePartyComponent partyTwo) 
+    List<BattleSite> mBattleSites;
+    public void StartBattle(BattlePartyComponent playerParty, BattlePartyComponent enemyParty) 
     {
-        Debug.Log($"Starting Battle betweem: {partyOne.gameObject.name} and {partyTwo.gameObject.name}");
+        if (mBattleSites == null) 
+        {
+            mBattleSites = new List<BattleSite>();
+            mBattleSites.AddRange(GameObject.FindObjectsByType<BattleSite>(FindObjectsSortMode.None)); ;
+        }
+        Debug.Log($"Starting Battle between: {playerParty.gameObject.name} and {enemyParty.gameObject.name}");
+        PrepParty(playerParty);
+        PrepParty(enemyParty);
+    }
+
+    private void PrepParty(BattlePartyComponent party) 
+    {
+        BattleSite partyBattleSite = mBattleSites.Find((battleSite) => { return !battleSite.IsPlayerSite; });
+        if (party.gameObject.CompareTag("Player")) 
+        {
+            partyBattleSite = mBattleSites.Find((battleSite) => { return battleSite.IsPlayerSite; });
+        }
+        int i = 0;
+        foreach (BattleCharacter partyBattleCharacter in party.GetBattleCharacters()) 
+        {
+            partyBattleCharacter.transform.position = partyBattleSite.GetPositionForUnit(i);
+            partyBattleCharacter.transform.rotation = partyBattleSite.transform.rotation;
+            i++;
+        }
     }
 }
